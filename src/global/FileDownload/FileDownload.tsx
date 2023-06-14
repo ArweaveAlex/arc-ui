e92959a06292577e7e668339d15a1f5af83a5462
NickJ202 <nickjuliano20@gmail.com>
2023-06-14 11:39:23 -0400

    temp remove download

--- src/global/FileDownload/FileDownload.tsx
+++ src/global/FileDownload/FileDownload.tsx
@@ -1,21 +1,25 @@
-import { Button } from 'components/atoms/Button';
-import { language } from 'helpers/language';
+// import { Button } from 'components/atoms/Button';
+// import { language } from 'helpers/language';
 
 import { IProps } from './types';
 
+// @ts-ignore
 export default function FileDownload(props: IProps) {
-	function handleDownload() {
-		if (props.fileUrl) {
-			const link = document.createElement('a');
-			link.href = props.fileUrl;
-			link.download = 'file';
-			document.body.appendChild(link);
-			link.click();
-			document.body.removeChild(link);
-		}
-	}
+	// function handleDownload() {
+	// 	if (props.fileUrl) {
+	// 		const link = document.createElement('a');
+	// 		link.href = props.fileUrl;
+	// 		link.download = 'file';
+	// 		document.body.appendChild(link);
+	// 		link.click();
+	// 		document.body.removeChild(link);
+	// 	}
+	// }
 
-	return (
-		<Button type={'alt1'} label={language.download} handlePress={() => handleDownload()} height={52.5} width={275} />
-	);
+	// TODO: fix download
+	return null;
+
+	// return (
+	// 	<Button type={'alt1'} label={language.download} handlePress={() => handleDownload()} height={52.5} width={275} />
+	// );
 }
